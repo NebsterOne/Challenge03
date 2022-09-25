1a504eec9a8f71909a4857483dbe1048b0418ffb
N Z <nelectrik@gmail.com>
2022-09-25 23:04:10 +0930

    Added arrays list for Numbers, Symbols, LowerCase, UpperCase and if selected any of them, add them to the box, from where the random will be taken

--- Develop/script.js
+++ Develop/script.js
@@ -34,7 +34,32 @@ function generatePassword(){
   var userSelectedLowercase = window.confirm("Include lower case in your password? Click Ok to confirm or Cancel to ignore")  
   var userSelectedUppercase = window.confirm("Include upper case in your password? Click Ok to confirm or Cancel to ignore") 
 
+
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolsList = ["!", "#", "$", "%", "&", "'", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", ",", "^", "_", "{", "|", "}", "~"] 
+  var lowerCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var upperCaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+
+  var optionsSelectedBox = [] 
+
+  if (userSelectedNumbers === true){
+    optionsSelectedBox.push(numberList)
+  }
+
+  if (userSelectedSymbols === true){
+    optionsSelectedBox.push(symbolsList)
+  }
+
+  if (userSelectedLowercase === true){
+    optionsSelectedBox.push(lowerCaseList)
+  }
+  if (userSelectedUppercase === true){
+    optionsSelectedBox.push(upperCaseList)
+   }
+
 }  
+
 
 
 
